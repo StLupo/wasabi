35597e4fa59f57220e0b3f1581d328d35cb61e1b
StLupo <jash-public@sherlocks-home.net>
2015-11-21 01:01:47 +0100

    Initial Checkin
    Initial checkin of a raspi + python digital picture frame + alarm clock

---
+++ display/main.py
@@ -0,0 +1,255 @@
+# -*- coding: utf-8 -*-
+
+
+import sys
+import time
+import os
+import random
+import urllib2
+import pygame
+import shutil
+
+from PyQt4 import QtCore, QtGui
+
+import kaja_clock
+import yr_info 
+#from sip import enableautoconversion
+
+PHOTO_DIR_1 = "/home/pi/git/wasabi/photos/ski"
+PHOTO_DIR_2 = "/home/pi/git/wasabi/photos/cats"
+WEATHER_URL = "http://www.yr.no/place/Norway/Buskerud/Kongsberg/Kongsberg/"
+ALARM_LIGHT_ON = "/home/pi/git/wasabi/cache/alarm_on.png"
+ALARM_LIGHT_OFF = "/home/pi/git/wasabi/cache/alarm_off.png"
+ALARM_DELAYED = "/home/pi/git/wasabi/cache/alarm_blue.png"
+BACKGROUND_IMAGE = "/home/pi/git/wasabi/backgrounds/night/night3.png"
+DAY_BACKGROUND_FOLDER = "/home/pi/git/wasabi/backgrounds/pink"
+NIGHT_BACKGROUND_FOLDER = "/home/pi/git/wasabi/backgrounds/night"
+DARK_HOUR_START = 20
+DARK_HOUR_END = 7
+ALARM_SOUND = "/home/pi/git/wasabi/audio/01_diho.mp3"
+ALARM_FILE = "/tmp/walarm"
+
+PHOTO_PERIOD_1 = 60
+PHOTO_PERIOD_2 = 60
+WEATHER_QUERY_PERIOD = 15*60
+
+class MyMainWindow(QtGui.QMainWindow):
+ 
+    def __init__(self):
+        QtGui.QMainWindow.__init__(self)
+        self._show_colon = True
+        self._ctr = 0
+        self._is_night = False
+        self._is_day = False
+        
+        self._set_volume(85)
+        self._enable_audio_jack(False)
+        pygame.mixer.init()
+        pygame.mixer.music.load(ALARM_SOUND)
+ 
+    def start(self, ui):
+        
+        #Link to the UI
+        self._ui = ui
+        
+        #Our Timer
+        self._timer = QtCore.QTimer(self)
+        self._timer.timeout.connect(self.time)
+        self._timer.start(1000)
+        
+        #Background
+        hour = int(time.strftime("%H"))
+        if hour >= 7 and hour <=20:
+            self._is_night = False
+        else:
+            self._is_night = True
+        self._update_background(self._is_night)
+        
+        #Hide the mouse pointer
+        self._hide_pointer()
+        
+    def _update_background(self, is_night):
+        
+        self._ui.centralwidget.setStyleSheet("QWidget { color : white; }")
+        palette = QtGui.QPalette()
+        if is_night:
+            photo_path = self._random_photo_path(NIGHT_BACKGROUND_FOLDER)
+            self._ui.clock.setStyleSheet("QLabel {color:#606060;}")
+        else:
+            photo_path = self._random_photo_path(DAY_BACKGROUND_FOLDER)
+            self._ui.clock.setStyleSheet("QLabel {color:#ffffff;}")
+        palette.setBrush(self.backgroundRole(), QtGui.QBrush(QtGui.QImage(photo_path)))
+        self.setPalette(palette)
+        
+    def _random_photo_path(self, photo_dir):
+        
+        files = os.listdir(photo_dir)
+        idx = random.randint(0,len(files) - 1)
+        return os.path.join(photo_dir, files[idx])
+        
+    def _random_photo_in_label(self, photo_dir, label):
+        
+        if self._is_night:
+            label.clear()
+        else:
+            files = os.listdir(photo_dir)
+            idx = random.randint(0,len(files) - 1)
+            label.setPixmap(QtGui.QPixmap(self._random_photo_path(photo_dir)))
+            label.setScaledContents(True)
+        
+    def _check_weather(self):
+        
+        ywf = yr_info.YrWeatherFetcher()
+        ywf.open(WEATHER_URL)
+        ret = ywf.get_result()
+        
+        for i, info in enumerate(ret):
+            time_label = [self._ui.yr_time_1, self._ui.yr_time_2, self._ui.yr_time_3, self._ui.yr_time_4][i]
+            temp_label = [self._ui.yr_temp_1, self._ui.yr_temp_2, self._ui.yr_temp_3, self._ui.yr_temp_4][i]
+            gphx_label = [self._ui.yr_precip_1, self._ui.yr_precip_2, self._ui.yr_precip_3, self._ui.yr_precip_4][i]
+            time_label.setText(info[0].decode("utf-8"))
+            if info[1][0] in ["-", "0"]:
+                temp_label.setStyleSheet("QLabel {color:#6495ed;}")
+            else:
+                temp_label.setStyleSheet("QLabel {color:red;}")
+            temp_label.setText(info[1].decode("utf-8"))
+            gphx_label.setPixmap(QtGui.QPixmap(info[2]))
+        
+    def _update_alarm(self):
+        
+        alarm_string = "No Alarm"
+        alarm_light = ALARM_LIGHT_OFF
+        _f = os.popen("crontab -l")
+        lines = _f.readlines()
+        for line in lines:
+            if line.count(ALARM_FILE) and line[0] != "#":
+                alarm_light = ALARM_DELAYED
+                elements = line.split()
+                minute = int(elements [0])
+                hour = int(elements[1])
+                alarm_string = "Alarm:  %02i:%02i" % (hour, minute)
+                days = elements[4]
+                if days.count("-"):
+                    start_day, end_day = days.split("-")
+                    days = range(int(start_day), int(end_day) + 1)
+                else:
+                    days = [int(el) for el in days.split(",")]
+                now_day = int(time.strftime("%w"))
+                now_hour = int(time.strftime("%H"))
+                now_minute = int(time.strftime("%M"))
+                if days.count(now_day + 1):
+                    alarm_light = ALARM_LIGHT_ON
+                    break
+                elif days.count(now_day):
+                    if now_hour < hour:
+                        alarm_light = ALARM_LIGHT_ON
+                        break
+                    elif now_hour == hour and now_minute < minute:
+                        alarm_light = ALARM_LIGHT_ON
+                        break
+        self._ui.alarm_light.setPixmap(QtGui.QPixmap(alarm_light))   
+        self._ui.alarm_time.setText(alarm_string) 
+        
+    def _check_alarm(self):
+        
+        if os.path.exists(ALARM_FILE):
+            print "Found Alarm!!!"
+            #Remove the alarm file so we don't keep alarming
+            os.remove(ALARM_FILE)
+            #Set the volume back to 100%
+            self._enable_audio_jack(True)
+            pygame.mixer.music.play(1)
+        else:
+            pass
+            
+    def _set_volume(self, pct):
+        
+        vol_cmd = "amixer cset numid=1 -- %i%%" % (pct)
+        os.system(vol_cmd)
+        
+    def _enable_audio_jack(self, enable):
+          
+        if enable:
+            print "Enabling Audio Jack"
+            cmd = "sudo amixer cset numid=3 1"
+        else:
+            print "Disabling Audio Jack"
+            cmd = "sudo amixer cset numid=3 2"
+        os.system(cmd)
+        
+    def _check_playback(self):
+        
+        if not pygame.mixer.music.get_busy():
+            print "Not Busy"
+            self._enable_audio_jack(False)
+        else:
+            print "Busy"
+            
+    def mousePressEvent(self, QMouseEvent):
+        
+        pygame.mixer.music.stop()
+    
+    def _hide_pointer(self):
+        
+        #QtGui.QCursor.setPos(QtCore.QPoint(800, 480))
+        self.setCursor(QtCore.Qt.BlankCursor)
+            
+    def time(self):
+        
+        #Show Time
+        if self._show_colon:
+            self._ui.clock.setText(time.strftime("%H"+":"+"%M."))
+        else:
+            self._ui.clock.setText(time.strftime("%H"+":"+"%M"))
+        self._show_colon = not self._show_colon
+            
+        #Show Date
+        self._ui.date.setText(time.strftime("%A  %d. %m %Y"))
+        
+        #Show Alarm
+        self._update_alarm()
+        
+        #Check Alarm
+        self._check_alarm()
+        
+        #Check sound playback and shut the jack up if nothing playing
+        self._check_playback()
+              
+            
+        #Update photos
+        if not self._ctr % PHOTO_PERIOD_1:
+            self._random_photo_in_label(PHOTO_DIR_1, self._ui.graphics_upper)
+        if not self._ctr % PHOTO_PERIOD_2:
+            self._random_photo_in_label(PHOTO_DIR_2, self._ui.graphics_lower)
+        
+        #Update weather
+        if not (self._ctr - 1) % WEATHER_QUERY_PERIOD:
+            self._check_weather()
+            
+        #Update Background
+        hour = int(time.strftime("%H"))
+        if hour >= DARK_HOUR_END and hour <=DARK_HOUR_START:
+            if self._is_night:
+                self._is_night = False
+                self._update_background(self._is_night)
+                print "Change to day"
+        else:
+            if not self._is_night:
+                self._is_night = True
+                self._update_background(self._is_night)
+                print "Change to night"
+            
+        self._ctr += 1
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    window = MyMainWindow()
+    ui = kaja_clock.Ui_MainWindow()
+    ui.setupUi(window)
+    window.start(ui)
+    window.show()
+ 
+    sys.exit(app.exec_())
+ 
+if __name__ == "__main__":
+    main()
---
+++ display/yr_info.py
@@ -0,0 +1,123 @@
+
+import os
+from HTMLParser import HTMLParser
+from htmlentitydefs import name2codepoint
+import urllib2
+#from aifc import data
+
+PIX_CACHE = "/home/pi/git/wasabi/cache"
+
+class YrParser(HTMLParser):
+    
+    
+    def init(self):
+        self._in_table = 0
+        self._in_today_body = False
+        self._weather_col_index = 0
+        self._today = False
+        self._weather_info = [] #list of tuples, where 0:timestring, 1:temperature, 2:url to precip graphic
+        self._current_time = ""
+        self._current_temp = ""
+        self._current_pic_url = ""
+         
+    def get_weather_info(self):
+        
+        return self._weather_info
+        
+    
+    def handle_starttag(self, tag, attrs):
+        
+        if tag == "table":
+            self._in_table += 1
+        
+        if tag == "tbody" and self._today:
+            self._in_today_body = True
+            
+        if tag == "tr" and self._in_today_body:
+            self._weather_col_index = 0
+            self._current_tuple = ()
+        
+        if tag == "td" and self._in_today_body:
+            self._weather_col_index += 1
+            if self._weather_col_index == 3:
+                if attrs[0][1].count("plus"):
+                    self._current_temp = "+"
+                else:
+                    self._current_temp = ""
+        
+        if tag == "img" and self._weather_col_index == 2:
+            for attr in attrs:
+                if attr[0] == "src":
+                    self._current_pic_url = attr[1] 
+                
+    def handle_endtag(self, tag):
+        
+        if tag == "table":
+            self._in_table -= 1
+            if self._today:
+                self._today = False
+        if tag == "tr":
+            if self._in_today_body:
+                self._weather_info.append([self._current_time, self._current_temp, self._current_pic_url])
+        if tag == "tbody":
+            self._in_today_body = False
+        
+    def handle_data(self, data):
+        
+        if self._in_today_body:
+            if self._weather_col_index == 1:
+                if data.strip():
+                    self._current_time = data
+            elif self._weather_col_index == 3:
+                if data.strip():
+                    self._current_temp += data
+        elif self._in_table and (data.count("Today,") or data.count("Tomorrow,")):
+            self._today = True
+
+    
+    def handle_comment(self, data):
+        pass
+    
+#     def handle_entityref(self, name):
+#         c = unichr(name2codepoint[name])
+#         print "Named ent:", c
+#     
+#     def handle_charref(self, name):
+#         if name.startswith('x'):
+#             c = unichr(int(name[1:], 16))
+#         else:
+#             c = unichr(int(name))
+#         print "Num ent  :", c
+#     def handle_decl(self, data):
+#         print "Decl     :", data
+
+class YrWeatherFetcher(object):
+    
+    def __init__(self):
+        
+        self._parser = YrParser()
+        self._parser.init()
+        pass
+    
+    def open(self, url):
+        
+        opener = urllib2.urlopen(url)
+        self._parser.feed(opener.read())
+        
+    def get_result(self):
+        
+        result = self._parser.get_weather_info()
+        for i in range(len(result)):
+            opener = urllib2.urlopen(result[i][2])
+            target_path = os.path.join(PIX_CACHE, os.path.basename(result[i][2]))
+            with open(target_path, "w") as _f:
+                _f.write(opener.read())
+            result[i][2] = target_path
+        return result[:4]
+         
+if __name__ == '''__main__''':
+    ywf = YrWeatherFetcher()
+    ywf.open("http://www.yr.no/place/Norway/Buskerud/Kongsberg/Kongsberg/")
+    for el in  ywf.get_result():
+        for l in el:
+            print l
